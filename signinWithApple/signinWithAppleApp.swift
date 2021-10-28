ab149f52951d32d3693b2d56088d6a55ae0f4cb2
Andy Marthin <mr.andymarthin@gmail.com>
2021-10-28 16:09:08 +0700

    Initial Commit

---
+++ signinWithApple/signinWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  signinWithAppleApp.swift
+//  signinWithApple
+//
+//  Created by Andy Marthin on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct signinWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
